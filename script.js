7a987d5f487747cefc200f5b57a15913defe8968
lakekimhandsome <115394409+lakekimhandsome@users.noreply.github.com>
2025-05-18 04:45:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => {
+  key.addEventListener("click", () => {
+    let note = key.dataset.note;
+
+    // 샵 표기 변경: # → s
+    note = note.replace("#", "s");
+
+    const audio = new Audio(`sounds/${note}.wav`);
+    audio.play();
+  });
+});
